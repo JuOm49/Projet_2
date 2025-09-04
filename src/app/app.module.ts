1d0ca6829ba3e155f47a9c429c634b89afe97feb
Julien Omedes <julien.omedes@outlook.fr>
2025-09-04 11:32:23 +0200

    v1.0, add paths, change Models, change any type with strong typing, start home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+import { AppRoutingModule } from '@app/app-routing.module';
+import { AppComponent } from '@app/app.component';
+import { HomeComponent } from '@pages/home/home.component';
+import { NotFoundComponent } from '@pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [CommonModule, BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -1,13 +1,4 @@
-// TODO: create here a typescript interface for an olympic country
-/*
-example of an olympic country:
-{
-    id: 1,
-    country: "Italy",
-    participations: []
-}
-*/
-import { Participation } from './Participation';
+import { Participation } from '@core/models/Participation';
 
 export class Olympic {
     id!: number;

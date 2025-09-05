74b68afcc5c9e39c1e5597b2c13cfb3ed40055e1
Julien Omedes <julien.omedes@outlook.fr>
2025-09-05 15:26:09 +0200

    v1.O.2_dashboard2 finish details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 // import echarts core
 import * as echarts from 'echarts/core';
 import { CanvasRenderer } from 'echarts/renderers';
 import { NgxEchartsModule } from 'ngx-echarts';
-import { TooltipComponent, LegendComponent } from 'echarts/components';
-import { PieChart} from 'echarts/charts';
+import { TooltipComponent, LegendComponent, GridComponent } from 'echarts/components';
+import { LineChart, PieChart} from 'echarts/charts';
 
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/app.component';
@@ -15,11 +16,18 @@ import { DashboardComponent } from '@components/dashboard/dashboard.component';
 import { DashboardDetailsComponent } from '@components/dashboard-details/dashboard-details.component';
 import { NotFoundComponent } from '@components/not-found/not-found.component';
 
-echarts.use([PieChart, CanvasRenderer, TooltipComponent, LegendComponent]);
+echarts.use([PieChart, CanvasRenderer, TooltipComponent, LegendComponent, LineChart, GridComponent]);
 
 @NgModule({
   declarations: [AppComponent, DashboardComponent, DashboardDetailsComponent, NotFoundComponent],
-  imports: [NgxEchartsModule.forRoot({echarts }), BrowserModule, AppRoutingModule, MatCardModule, HttpClientModule],
+  imports: [
+    NgxEchartsModule.forRoot({echarts }),
+    BrowserModule,
+    AppRoutingModule,
+    MatCardModule,
+    RouterLink,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/dashboard-details/dashboard-details.component.ts
+++ src/app/components/dashboard-details/dashboard-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { Observable, tap } from 'rxjs';
+import { filter, Observable, take, tap } from 'rxjs';
 
 import { EChartsOption } from 'echarts';
 
@@ -17,16 +17,76 @@ export class DashboardDetailsComponent implements OnInit {
 
   olympicById$!: Observable<Olympic | null | undefined>;
   public lineOlympic!: EChartsOption;
+  public title!: string;
+  public numberEntriesText: string = 'Number of entries';
+  public totalNumberMedalsText: string = 'Total number medals';
+  public totalNumberAtheletesText: string = 'Total number of athletes';
+  public numberOfEntries: number = 0;
+  public totalNumberMedals: number = 0;
+  public totalNumberAthletes: number = 0;
+
+  private tabYears: number[] = [];
+  private seriesData: {value: number, city: string}[] = [];
 
   constructor(private route: ActivatedRoute, private olympicsService: OlympicService) { }
 
   ngOnInit(): void {
     const countryId: number = +this.route.snapshot.params['id'];
     this.olympicById$ = this.olympicsService.getOlympicById(countryId);
+    
 
-    // this.olympicById$.pipe(
-    //   tap((data) => console.log('Country data from service:', data))
-    // ).subscribe();
+    this.olympicById$.pipe(
+      filter(olympic => !!olympic),
+      take(1),
+      tap((olympic) => {
+        this.getOlympicCountryInformation(olympic);
+      }
+      )
+    ).subscribe();
   }
 
+  getOlympicCountryInformation(olympic: Olympic | null | undefined) {
+    if(!olympic) return;
+
+    this.title = olympic?.country;
+    this.numberOfEntries = olympic?.participations.length;
+    this.totalNumberAthletes = olympic?.participations.flatMap(participation => participation.athleteCount).reduce((acc, count) => acc + count, 0);
+    this.totalNumberMedals = olympic?.participations.flatMap(participation => participation.medalsCount).reduce((acc, count) => acc + count, 0);
+    for (let participation of olympic?.participations || []) {
+          this.tabYears.push(participation.year);
+          this.seriesData.push({
+            value: participation.medalsCount,
+            city: participation.city
+          });
+        }
+
+    this.lineOlympic = {
+      tooltip: {
+        trigger: 'axis',
+        formatter: (params: any) => {
+          const param = params[0];
+          return `Year : ${param.axisValue}<br>
+          Medals : ${param.data.value}<br>
+          City : ${param.data.city}`;
+        }
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: this.tabYears
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          type: 'line',
+          data: this.seriesData
+        }
+      ]
+    };
+  }
 }
